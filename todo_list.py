e5e14f2c883dbcdab1cb3b906cf40370a8400e8c
ConniConni <nmtgktmo@gmail.com>
2025-07-10 19:35:56 +0900

    feat: ログ設定を呼び出す #13

--- todo_list.py
+++ todo_list.py
@@ -1,5 +1,10 @@
 from todo_list import main
+from todo_list import logger_config
 
 
 if __name__ == "__main__":
+
+    # アプリケーションのログ設定処理を呼び出す
+    logger_config.setup_logging()
+    # アプリのエントリーポイントを実行する
     main.run_app()
