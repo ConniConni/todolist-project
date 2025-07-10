05510948b1ffd0ca52013091a0e1c8c38d5cf940
ConniConni <nmtgktmo@gmail.com>
2025-07-10 22:30:45 +0900

    feat: CSV操作の正常系・異常系ログの追加  #13

--- todo_list/utils/file_handler.py
+++ todo_list/utils/file_handler.py
@@ -1,23 +1,32 @@
 import csv
-
+import logging
 
 CSV_FILE = "todo.csv"
+
+logger = logging.getLogger(__name__)
 
 
 def load_tasks():
     """CSVファイルを読み込み、読み取ったタスクをリストに追加し返す関数"""
+    logger.info("CSVファイル読み込み処理を開始しました。")
 
     try:
         with open(CSV_FILE, "r", encoding="utf-8") as f:
             reader = csv.reader(f)
             return [row[0] for row in reader if row]
 
-    except FileNotFoundError:
+    except FileNotFoundError as e:
+        logger.error(f"{e}: ファイルの読み取りに失敗しました。")
         return []
+
+    finally:
+        logger.info("CSVファイル読み込み処理を終了しました。")
 
 
 def save_tasks(task_list):
     """task_listのタスクをCSVに書き込む関数"""
+
+    logger.info("CSVファイル書き込み処理を開始しました。")
 
     with open(CSV_FILE, "w", encoding="utf-8") as f:
         writer = csv.writer(f)
@@ -25,3 +34,5 @@ def save_tasks(task_list):
             # writerow()メソッドはイテラブルな引数を要求する
             # 文字列もイテラブルなため、リストにしないとタスクが1文字ずつカンマ区切りで書き込まれてしまう
             writer.writerow([task])
+
+    logger.info("CSVファイル書き込み処理を終了しました。")
